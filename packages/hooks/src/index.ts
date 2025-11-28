3f2832e473b8e85331c4e1a03bda4d2357a2abbb
陈健聪cjc <chenjiancong@sutpc.com>
2025-11-28 17:17:33 +0800

    feat(hooks 新增判断对象函数): hooks 新增判断对象函数

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,3 +1,8 @@
 export const isFunction = (val: object) => {
   return typeof val === "function";
 };
+
+// 判断是否为对象
+export const isObject = (val: object) => {
+  return val !== null && typeof val === "object";
+};
